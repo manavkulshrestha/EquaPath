8ca80c3e10c537150a4ea415c7a74722b8815963
manavkulshrestha <manavkoolz@gmail.com>
2018-08-16 00:05:36 -0400

    Functions now remove terms with coefficient 0

--- derivitive.py
+++ derivitive.py
@@ -22,7 +22,7 @@
 # Input must be formatted like '4x^5 -2x^2 +3'
 # Output gives polynomial in processable list form [[4, 5], [-2, 2], [3, 1]]
 def y_list(y_string):
-	return [[int(num) for num in term.split('x^')] for term in y_string.split(' ')]
+	return [[int(num) for num in term.split('x^')] for term in y_string.split(' ') if term[0] is not '0']
 	# y_list = []
 
 
@@ -34,10 +34,10 @@ def y_list(y_string):
 
 # Input must be in processable list form
 def y_string(y_list):
-	return ' '.join([f'{term[0]}x^{term[1]}' for term in y_list])
+	return ' '.join([f'{term[0]}x^{term[1]}' for term in y_list if term[0] != 0])
 
 
 # Input must be the processable list form [[4, 5], [-2, 2], [3, 1]]
 # Outputs pretty string form of polynomial '4x^5 -2x^2 +3'
 def derivitive(y_list):#
-	return [[y[0]*y[1], y[1]-1] for term in y]
+	return [[y[0]*y[1], y[1]-1] for term in y if y[0] != 1]
