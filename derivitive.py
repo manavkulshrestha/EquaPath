08c7c18ddb106a66f4843079408d1af0fc6fad92
manavkulshrestha <manavkoolz@gmail.com>
2018-08-18 23:38:58 -0400

    Fixed sign not appearing for positive terms

--- derivitive.py
+++ derivitive.py
@@ -1,3 +1,5 @@
+import pdb;
+
 #String normalizer for more user friendly string inputs
 def normalize_y_string(y_string):
 	if y_string[0] not in ('-', '+'):# if sign not specified at the begining of the polynomial (term is +ve)
@@ -19,22 +21,35 @@ def get_y_list(y_string):
 
 # Input must be in processable list form
 def get_y_string(y_list):
-	return ' '.join([f'{term[0]}x^{term[1]}' for term in y_list if term[0] != 0])
+	return ' '.join([f'{"+" if term[0] > 0 else ""}{term[0]}x^{term[1]}' for term in y_list if term[0] != 0])
 
 # Input must be the processable list form [[4, 5], [-2, 2], [3, 1]]
 # Outputs pretty string form of polynomial '4x^5 -2x^2 +3'
 def get_derivitive(y_list):
-	return [[term[0]*term[1], term[1]-1] for term in y_list if term[0] != 1]
+	return [[term[0]*term[1], term[1]-1] for term in y_list if not (term[1] == 0 or term[0] == 0)]
 
-##Testing
-# y_string = '4x^5 -2x^2 +x +3'
-# y_list = get_y_list(y_string)
+def get_value_at(x,y_list):
+	print(f'eval {y_list} at {x}')
+	return sum((x**term[1])*term[0] for term in y_list)
 
-# print('y:\n',y_string)
-# print(y_list)
+#Testing
+y_string = '4x^5 -2x^2 +x +3'
+y_list = get_y_list(y_string)
 
-# yprime_list = get_derivitive(y_list)
-# yprime_string = get_y_string(yprime_list)
+print('y:\n',y_string)
+print(y_list)
+print('y(0): ',get_value_at(0,y_list))
+print('y(5): ',get_value_at(5,y_list))
 
-# print('y\':\n',yprime_string)
-# print(yprime_list)
+yprime_list = get_derivitive(y_list)
+yprime_string = get_y_string(yprime_list)
+
+print('y\':\n',yprime_string)
+print(yprime_list)
+print('y(0): ',get_value_at(0,yprime_list))
+pdb.set_trace()
+print('y(5): ',get_value_at(5,yprime_list))
+
+# y2 = [[20, 4], [-4, 1], [1, 0]]
+# print('normalizing: ',[[20, 4], [-4, 1], [1, 0]])
+# print('normalized_y_string: ',normalize_y_string([[20, 4], [-4, 1], [1, 0]]))
